cce6fc579f6c86106cc58cbd806e8519d1f42b99
Soxoj <31013580+soxoj@users.noreply.github.com>
2022-06-04 16:32:32 -0400

    Create addons.sh

---
+++ addons.sh
@@ -0,0 +1,6 @@
+sudo apt install keepassx 
+wget https://github.com/TheAssassin/AppImageLauncher/releases/download/continuous/appimagelauncher_2.2.0-gha100.5a0fdec+bionic_amd64.deb
+sudo dpkg -i appimagelauncher_2.2.0-gha100.5a0fdec+bionic_amd64.deb
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.14.6/Obsidian-0.14.6.AppImage
+chmod +x ./Obsidian-0.14.6.AppImage
+./Obsidian-0.14.6.AppImage
